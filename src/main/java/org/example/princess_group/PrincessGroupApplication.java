ff11310bf487dbe1efd1819c2cca42991885b378
doyoon <doyoun0527@naver.com>
2023-12-25 16:58:56 +0900

    init commit

---
+++ src/main/java/org/example/princess_group/PrincessGroupApplication.java
@@ -0,0 +1,13 @@
+package org.example.princess_group;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrincessGroupApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PrincessGroupApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/example/princess_group/PrincessGroupApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.princess_group;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrincessGroupApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
