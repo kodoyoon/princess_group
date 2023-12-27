90dd2757e190c68667fa67a8f57fdbcf001ee4eb
yudonggeun <ydong98@gmail.com>
2023-12-27 10:22:06 +0900

    Feat : Card 엔티티 필드 추가
    - 이름, 설명, 색상, 마감일, 순서 추가
    - 빌더 패턴 적용

--- src/main/java/org/example/princess_group/domain/card/entity/Card.java
+++ src/main/java/org/example/princess_group/domain/card/entity/Card.java
@@ -1,9 +1,33 @@
 package org.example.princess_group.domain.card.entity;
 
 import jakarta.persistence.Entity;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.example.princess_group.global.entity.BaseEntity;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Card extends BaseEntity {
 
+    private Long columnId;
+    private String name;
+    private String description;
+    private String color;
+    private Integer order;
+    private LocalDateTime deadline;
+
+    @Builder
+    public Card(Long columnId, String name, String description, String color, Integer order,
+        LocalDateTime deadline) {
+        this.columnId = columnId;
+        this.name = name;
+        this.description = description;
+        this.color = color;
+        this.order = order;
+        this.deadline = deadline;
+    }
 }
