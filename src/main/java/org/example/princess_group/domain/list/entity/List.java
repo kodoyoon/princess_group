7613ead7a0db2edfd017badb9110e03f8f958f52
Binsreoun <69248377+Binsreoun@users.noreply.github.com>
2023-12-27 10:17:04 +0900

    Feat : List Entity 생성

--- src/main/java/org/example/princess_group/domain/list/entity/List.java
+++ src/main/java/org/example/princess_group/domain/list/entity/List.java
@@ -1,9 +1,22 @@
 package org.example.princess_group.domain.list.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Builder;
+import org.example.princess_group.domain.board.entity.Board;
 import org.example.princess_group.global.entity.BaseEntity;
 
 @Entity
 public class List extends BaseEntity {
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id", nullable = false)
+    private Board board;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private int order;
 
 }
