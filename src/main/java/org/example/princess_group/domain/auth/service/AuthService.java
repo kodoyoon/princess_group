dfe800640091e0740f29fb70d573ba60b22d139b
yudonggeun <ydong98@gmail.com>
2023-12-29 08:17:50 +0900

    Feat : 권한 타입 정의
    보드, 리스트, 카드, 댓글에 대한 권한을 구체화하였습니다.

--- src/main/java/org/example/princess_group/domain/auth/service/AuthService.java
+++ src/main/java/org/example/princess_group/domain/auth/service/AuthService.java
@@ -1,9 +1,10 @@
 package org.example.princess_group.domain.auth.service;
 
 import org.example.princess_group.domain.auth.entity.DomainType;
+import org.example.princess_group.domain.auth.entity.ServiceAuthority;
 
 public interface AuthService {
 
-    boolean hasAuthority(DomainType type, ServiceAuthority authority);
+    boolean hasAuthority(DomainType type, ServiceAuthority authority, Long domainId);
 
 }
--- src/main/java/org/example/princess_group/domain/auth/service/AuthServiceImpl.java
+++ src/main/java/org/example/princess_group/domain/auth/service/AuthServiceImpl.java
@@ -1,11 +1,12 @@
 package org.example.princess_group.domain.auth.service;
 
 import org.example.princess_group.domain.auth.entity.DomainType;
+import org.example.princess_group.domain.auth.entity.ServiceAuthority;
 
 public class AuthServiceImpl implements AuthService {
 
     @Override
-    public boolean hasAuthority(DomainType type, ServiceAuthority authority) {
+    public boolean hasAuthority(DomainType type, ServiceAuthority authority, Long domainId) {
         return false;
     }
 }
