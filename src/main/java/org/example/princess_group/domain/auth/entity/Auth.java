26026cd86092e4fa4f1c9b0c8f242a8c77d79f56
yudonggeun <ydong98@gmail.com>
2023-12-29 08:45:16 +0900

    Feat : Auth 도메인 필드 구현

--- src/main/java/org/example/princess_group/domain/auth/entity/Auth.java
+++ src/main/java/org/example/princess_group/domain/auth/entity/Auth.java
@@ -1,11 +1,32 @@
 package org.example.princess_group.domain.auth.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 import org.example.princess_group.global.entity.BaseEntity;
 
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Auth extends BaseEntity {
 
+    @Column(nullable = false)
+    private Long userId;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ServiceAuthority authority;
+    @Enumerated(EnumType.STRING)
+    private DomainType type;
+    private Long domainId;
 
-
+    @Builder
+    private Auth(Long userId, ServiceAuthority authority, DomainType type, Long domainId) {
+        this.userId = userId;
+        this.authority = authority;
+        this.type = type;
+        this.domainId = domainId;
+    }
 }
