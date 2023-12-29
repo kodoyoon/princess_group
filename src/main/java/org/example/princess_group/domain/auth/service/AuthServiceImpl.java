2118c74b52344974e4c699edf95ff389c9669428
yudonggeun <ydong98@gmail.com>
2023-12-29 09:02:29 +0900

    Feat : 특정 유저 권한 목록 조회 구현(Service)

--- src/main/java/org/example/princess_group/domain/auth/service/AuthServiceImpl.java
+++ src/main/java/org/example/princess_group/domain/auth/service/AuthServiceImpl.java
@@ -1,11 +1,18 @@
 package org.example.princess_group.domain.auth.service;
 
 import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.example.princess_group.domain.auth.repository.AuthRepository;
 import org.example.princess_group.global.dto.AuthInfo;
 import org.example.princess_group.global.entity.DomainType;
 import org.example.princess_group.global.entity.ServiceAuthority;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
+
+    private final AuthRepository authRepository;
 
     @Override
     public boolean assignAuthority(Long userId, DomainType type, ServiceAuthority authority, Long domainId) {
@@ -14,6 +21,8 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     public List<AuthInfo> getAuthorityByUserId(Long userId) {
-        return null;
+        return authRepository.findAllByUserId(userId).stream()
+            .map(AuthInfo::of)
+            .toList();
     }
 }
